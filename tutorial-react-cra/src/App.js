5c0bff84f9707d01bedc6e0815fe1385f0c91569
BrucelD14 <samuderaduta144@gmail.com>
2022-11-02 13:54:44 +0700

    add more function in Welcome.js

--- tutorial-react-cra/src/App.js
+++ tutorial-react-cra/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import Welcome from './Welcome.js'
+import logo from "./logo.svg";
+import "./App.css";
+import Welcome, { WelcomeH2, WelcomeH3 } from "./Welcome";
 
 function App() {
   return (
     <div className="App">
       <Welcome />
+      <WelcomeH2 />
+      <WelcomeH3 />
     </div>
   );
 }
--- tutorial-react-cra/src/Welcome.js
+++ tutorial-react-cra/src/Welcome.js
@@ -1,5 +1,11 @@
-function Welcome() {
-    return <h1>Welcome</h1>
+export default function Welcome() {
+  return <h1>Welcome</h1>;
 }
 
-export default Welcome;
+export function WelcomeH2() {
+  return <h2>Welcome from H2</h2>;
+}
+
+export function WelcomeH3() {
+  return <h3>Welcome from H3</h3>;
+}
