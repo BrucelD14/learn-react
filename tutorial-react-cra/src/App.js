13da16b06b343f2eb1ba08df340fe1b40a281f60
BrucelD14 <samuderaduta144@gmail.com>
2022-11-03 11:19:08 +0700

    access image

--- tutorial-react-cra/src/App.js
+++ tutorial-react-cra/src/App.js
@@ -4,7 +4,7 @@ import Button from "./Button";
 function App() {
   return (
     <div className="App">
-      <Button>Click Aku</Button>
+      <img src="/logo512.png" alt="images content" />
     </div>
   );
 }
