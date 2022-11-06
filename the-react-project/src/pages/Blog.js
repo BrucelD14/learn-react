81e8f28ee46df8ba87a542a669995ac590cd9b84
BrucelD14 <samuderaduta144@gmail.com>
2022-11-06 22:09:33 +0700

    show article

--- the-react-project/src/pages/Blog.js
+++ the-react-project/src/pages/Blog.js
@@ -2,21 +2,34 @@ import { useEffect, useState } from "react";
 
 export default function Blog() {
   const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(function () {
     async function getArticles() {
       const request = await fetch(
         "https://api.spaceflightnewsapi.net/v3/articles"
       );
-      const response = request.json();
+      const response = await request.json();
 
       setArticles(response);
-      console.log(response);
     }
-
     getArticles();
+    setLoading(false);
   }, []);
 
-  return <h1>BLOG PAGE</h1>;
+  return (
+    <section>
+      <h1>BLOG</h1>
+      <p>Berikut ini adalah tulisan tulisanku (tapi bo'ong):</p>
+
+      {loading && <i>Loading articles...</i>}
+      {!loading && (
+        <div>
+          {articles.map(function (article) {
+            return <article key={article.id}>{article.title}</article>;
+          })}
+        </div>
+      )}
+    </section>
+  );
 }
