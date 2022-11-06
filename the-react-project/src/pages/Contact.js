fbbe3b3336ad7e02995d7b46777cbb29effd5d35
BrucelD14 <samuderaduta144@gmail.com>
2022-11-06 14:09:25 +0700

    add content at Contact.js

--- the-react-project/src/pages/Contact.js
+++ the-react-project/src/pages/Contact.js
@@ -1,3 +1,21 @@
 export default function Contact() {
-    return <h1>CONTACT PAGE</h1>
+  return (
+    <section>
+      <h1>CONTACT</h1>
+      <p>Kamu dapat menghubungiku dengan cara dibawah ini:</p>
+      <ul>
+        <li>WA: 088821121814</li>
+        <li>Email: brucel@developer.com</li>
+      </ul>
+      <p>Kamu dapat melihatku pansos di sosial media:</p>
+      <ul>
+        <li>
+          <a href="https://www.instagram.com/_samudera/">Instagram</a>
+        </li>
+        <li>
+          <a href="https://github.com/BrucelD14">GitHub</a>
+        </li>
+      </ul>
+    </section>
+  );
 }
