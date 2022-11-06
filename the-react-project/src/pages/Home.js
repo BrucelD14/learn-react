7434643638ecc908706776f329a09ff800c6b00d
BrucelD14 <samuderaduta144@gmail.com>
2022-11-06 13:56:06 +0700

    add content at Home.js

--- the-react-project/src/pages/Home.js
+++ the-react-project/src/pages/Home.js
@@ -1,3 +1,9 @@
 export default function Home() {
-    return <h1>HOME PAGE</h1>
+    return (
+        <section>
+            <h1>SELAMAT DATANG DI WEBSITE GUEH</h1>
+            <p>Halo semua, ini website baru aku. Kamu bisa mendapatkan informasi seputar pribadiku disini, salam kenal yaa. Have a nice day!</p>
+        </section>
+
+    )
 }
