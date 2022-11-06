f33ca03ede850e5cf719ce3f778a9e0c24ef6baf
BrucelD14 <samuderaduta144@gmail.com>
2022-11-06 13:50:22 +0700

    add nav on App,js

--- the-react-project/src/App.js
+++ the-react-project/src/App.js
@@ -1,20 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import Blog from './pages/Blog';
-import Contact from './pages/Contact';
+import "./App.css";
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div className="App">
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/profile' element={<Profile />} />
-      <Route path='/blog' element={<Blog />} />
-      <Route path='/contact' element={<Contact />} />
-    </Routes>
+      <nav>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link to="/blog">Blog</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
