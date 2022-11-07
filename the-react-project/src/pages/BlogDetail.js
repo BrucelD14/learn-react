515aadf5aef77e92285175f70bafabb88ac3c223
BrucelD14 <samuderaduta144@gmail.com>
2022-11-07 11:49:29 +0700

    add content on BlogDetail.js

--- the-react-project/src/pages/BlogDetail.js
+++ the-react-project/src/pages/BlogDetail.js
@@ -3,26 +3,37 @@ import { useParams } from "react-router-dom";
 
 export default function BlogDetail() {
   const params = useParams();
-  console.log(params);
   const [article, setArticle] = useState({});
   const [loading, setLoading] = useState(true);
 
-  useEffect(function () {
-    async function getArticle() {
-      const request = await fetch(
-        `https://api.spaceflightnewsapi.net/v3/articles/${params.id}`
-      );
-      const response = await request.json();
+  useEffect(
+    function () {
+      async function getArticle() {
+        const request = await fetch(
+          `https://api.spaceflightnewsapi.net/v3/articles/${params.id}`
+        );
+        const response = await request.json();
 
-      setArticle(response);
-      setLoading(false);
-    }
-    getArticle();
-  }, []);
+        setArticle(response);
+        setLoading(false);
+      }
+      getArticle();
+    },
+    [params]
+  );
 
   return (
     <section>
-      <h1>BLOG DETAIL</h1>
+      {loading && <i>Loading article...</i>}
+      <h2>{article.title}</h2>
+      <date>{new Date(article.publishedAt).toLocaleDateString()}</date>
+      <p>{article.summary}</p>
+      <p>
+        Source:{" "}
+        <a href={article.url} target="_blank">
+          {article.newsSite}
+        </a>
+      </p>
     </section>
   );
 }
