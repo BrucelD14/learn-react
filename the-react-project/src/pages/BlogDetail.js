53eced1cd78abdfdcc100b57f657fa63c676968a
BrucelD14 <samuderaduta144@gmail.com>
2022-11-07 12:29:55 +0700

    resize image

--- the-react-project/src/pages/BlogDetail.js
+++ the-react-project/src/pages/BlogDetail.js
@@ -24,19 +24,24 @@ export default function BlogDetail() {
 
   return (
     <section>
-      {loading && <i>Loading article...</i>}
-      <h2>{article.title}</h2>
-      <time>{new Date(article.publishedAt).toLocaleDateString()}</time>
-      <br />
-      <br />
-      <img src={article.imageUrl} alt={article.title} width={800} />
-      <p>{article.summary}</p>
-      <p>
-        Source:{" "}
-        <a href={article.url} target="_blank" rel="noreferrer">
-          {article.newsSite}
-        </a>
-      </p>
+      {loading ? (
+        <i>Loading article...</i>
+      ) : (
+        <article>
+          <h2>{article.title}</h2>
+          <time>{new Date(article.publishedAt).toLocaleDateString()}</time>
+          <br />
+          <br />
+          <img src={article.imageUrl} alt={article.title} width={700} />
+          <p>{article.summary}</p>
+          <p>
+            Source:{" "}
+            <a href={article.url} target="_blank" rel="noreferrer">
+              {article.newsSite}
+            </a>
+          </p>
+        </article>
+      )}
     </section>
   );
 }
