d6e99ed5cde33d5da84a148280973d34fc067807
BrucelD14 <samuderaduta144@gmail.com>
2022-11-07 11:35:51 +0700

    connect blog detail to API

--- the-react-project/src/pages/BlogDetail.js
+++ the-react-project/src/pages/BlogDetail.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 export default function BlogDetail() {
-    return(
-        <section>
-        <h1>BLOG DETAIL</h1>
-        </section>
-    )
+  const params = useParams();
+  console.log(params);
+  const [article, setArticle] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(function () {
+    async function getArticle() {
+      const request = await fetch(
+        `https://api.spaceflightnewsapi.net/v3/articles/${params.id}`
+      );
+      const response = await request.json();
+
+      setArticle(response);
+      setLoading(false);
+    }
+    getArticle();
+  }, []);
+
+  return (
+    <section>
+      <h1>BLOG DETAIL</h1>
+    </section>
+  );
 }
